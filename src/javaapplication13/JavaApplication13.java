7b18725da25a6f6a846be7ad0225130f3bfdbbc7
trinhnk <trinhnk.imlv@gmail.com>
2018-11-27 09:14:52 +0700

    This is a test

---
+++ src/javaapplication13/JavaApplication13.java
@@ -0,0 +1,16 @@
+package javaapplication13;
+
+/**
+ *
+ * @author trinhnk
+ */
+public class JavaApplication13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+    
+}
