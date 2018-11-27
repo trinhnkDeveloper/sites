1a92df6786c8bc2666f8e36e90ac97add0b3d9e9
trinhnk <trinhnk.imlv@gmail.com>
2018-11-27 09:29:19 +0700

    add another screen output line

--- src/javaapplication13/JavaApplication13.java
+++ src/javaapplication13/JavaApplication13.java
@@ -11,6 +11,7 @@ public class JavaApplication13 {
      */
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world again;");
     }
     
 }
